84d2f52e1aa5194927c28d11301093d7e743850d
Cristina Robinson <hellolovelygeek+github@gmail.com>
2018-04-30 21:27:51 -0700

    Footer adjustment

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 
 					*/ ?>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. All rights reserved. <br /> <a href="https://thelovelygeek.com/humans.txt">Crafted with &hearts; in California</a>.</p>
 
 				</div>
 
