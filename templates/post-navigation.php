99773a2e1fe866c93b14c991f6c49f4acc324d22
Cristina Robinson <hellolovelygeek+github@gmail.com>
2018-05-12 15:45:06 -0700

    Updated select fields and styled buttons
    Replaced default arrows on select fields with a custom icon. Style buttons.

--- templates/post-navigation.php
+++ templates/post-navigation.php
@@ -1,6 +1,8 @@
 <div class="post-navigation">
 	
-	<p><?php posts_nav_link('&#8734;','Newer Posts &rarr;','&larr; Older Posts'); ?></p>
+	<div class="previous_post_link"><?php previous_posts_link( '&larr; Older Posts' ); ?></div>
+	<div class="next_post_link"><?php next_posts_link( 'Newer Posts &rarr;', '' ); ?></div>
+	
 
 	<? /* php plate_page_navi(); */ ?>
 
