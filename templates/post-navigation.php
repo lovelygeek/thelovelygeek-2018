89a105a4b0b46df7b7d19f0f5364c21780432d3b
Cristina Robinson <hellolovelygeek+github@gmail.com>
2018-05-12 15:50:32 -0700

    Styled previous and next post links

--- templates/post-navigation.php
+++ templates/post-navigation.php
@@ -1,7 +1,7 @@
 <div class="post-navigation">
 	
-	<div class="previous_post_link"><?php previous_posts_link( '&larr; Older Posts' ); ?></div>
-	<div class="next_post_link"><?php next_posts_link( 'Newer Posts &rarr;', '' ); ?></div>
+	<div class="previous_post_link"><?php previous_posts_link( 'Newer Posts &rarr;', '' ); ?></div> <!--usually points to older entries (toward the end of the set)-->
+	<div class="next_post_link"><?php next_posts_link(  '&larr; Older Posts', '' ); ?></div> <!-- usually points to newer entries (toward the beginning of the set)-->
 	
 
 	<? /* php plate_page_navi(); */ ?>
