93b6705e73ad1263d2f2f2794cc5bf33def34d90
Cristina Robinson <hellolovelygeek+github@gmail.com>
2018-06-01 07:02:06 -0700

    Added tracking codes
    Google Analytics and HotJar

--- header.php
+++ header.php
@@ -58,7 +58,28 @@
 		<?php // end of wordpress head ?>
 
 		<?php // drop Google Analytics Here ?>
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+			<script async src="https://www.googletagmanager.com/gtag/js?id=UA-59769282-1"></script>
+			<script>
+			  window.dataLayer = window.dataLayer || [];
+			  function gtag(){dataLayer.push(arguments);}
+			  gtag('js', new Date());
+			
+			  gtag('config', 'UA-59769282-1');
+			</script>		
 		<?php // end analytics ?>
+
+		<!-- Hotjar Tracking Code for http://www.thelovelygeek.com -->
+		<script>
+		    (function(h,o,t,j,a,r){
+		        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		        h._hjSettings={hjid:266596,hjsv:6};
+		        a=o.getElementsByTagName('head')[0];
+		        r=o.createElement('script');r.async=1;
+		        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		        a.appendChild(r);
+		    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>		
 
 	</head>
 
