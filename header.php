ef393f7a262f63d1dec4f0ff84b7c184c171c7d4
Cristina Robinson <hellolovelygeek+github@gmail.com>
2018-07-04 09:07:39 -0700

    Added code for reCAPTCHA

--- header.php
+++ header.php
@@ -81,7 +81,10 @@
 		        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
 		        a.appendChild(r);
 		    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-		</script>		
+		</script>
+		
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>		
 
 	</head>
 
