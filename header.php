46abc2e932f89abf55d4b0f33cfa5eb6622454b7
Cristina Robinson <cristina@Cristinas-iMac.local>
2020-04-30 14:12:35 -0700

    Flodesk
    Added code for Flodesk form capture

--- header.php
+++ header.php
@@ -84,6 +84,23 @@
 		    google_ad_client: "ca-pub-6838826781799582",
 		    enable_page_level_ads: true
 		  });
+		</script>
+		
+		<!-- Flodesk -->
+		<script>
+		  (function(w, d, t, s, n) {
+		    w.FlodeskObject = n;
+		    var fn = function() {
+		      (w[n].q = w[n].q || []).push(arguments);
+		    };
+		    w[n] = w[n] || fn;
+		    var f = d.getElementsByTagName(t)[0];
+		    var e = d.createElement(t);
+		    var h = '?v=' + new Date().getTime();
+		    e.async = true;
+		    e.src = s + h;
+		    f.parentNode.insertBefore(e, f);
+		  })(window, document, 'script', 'https://assets.flodesk.com/universal.js', 'fd');
 		</script>		
 
 	</head>
