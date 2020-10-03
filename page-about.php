e20a5a5f265c2870144b1a4b7c9e67246cba5672
lovelygeek <lovelygeek@users.noreply.github.com>
2020-10-03 11:59:32 -0700

    About page edits
    Added Values section

--- page-about.php
+++ page-about.php
@@ -68,6 +68,18 @@
 </div>		
 	
 
+<div id="values" class="cf">
+	<div class="wrap">
+		<h3>Values I <span class="script">believe</span> in</h3>
+		<p>The way I operate is simple: be a decent human being. Everyone is welcome here.</p>
+		<div class="d-1of6 t-1of3 m-1of3"><img src="<?php echo get_theme_file_uri(); ?>/library/images/icon-science.svg" alt="science is real"><h5>science</h5><p>is real</p></div>
+		<div class="d-1of6 t-1of3 m-1of3"><img src="<?php echo get_theme_file_uri(); ?>/library/images/icon-blm.svg" alt="black lives matter"><h5>black</h5><p>lives matter</p></div>
+		<div class="d-1of6 t-1of3 m-1of3"><img src="<?php echo get_theme_file_uri(); ?>/library/images/icon-lgbtq.svg" alt="love is love"><h5>love</h5><p>always wins</p></div>
+		<div class="d-1of6 t-1of3 m-1of3"><img src="<?php echo get_theme_file_uri(); ?>/library/images/icon-womxn.svg" alt="pro choice"><h5>women's</h5><p>rights are human rights</p></div>
+		<div class="d-1of6 t-1of3 m-1of3"><img src="<?php echo get_theme_file_uri(); ?>/library/images/icon-liberty.svg" alt="humans are not illegal"><h5>humans</h5><p>are not illegal</p></div>
+		<div class="d-1of6 t-1of3 m-1of3"><img src="<?php echo get_theme_file_uri(); ?>/library/images/icon-kindness.svg" alt="kindness matters"><h5>kindness</h5><p>is everything</p></div>
+	</div>	
+</div>
 
 <div id="experience" class="cf">
 
